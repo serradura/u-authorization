6ad4ee358a885205c1e82812f026d661023a4fef
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-08-04 16:37:57 -0300

    Improve Micro::Authorization::Policy test

--- test/micro/authorization/policy_test.rb
+++ test/micro/authorization/policy_test.rb
@@ -2,62 +2,69 @@
 
 require 'test_helper'
 
-module Micro::Authorization::PolicyTest
+class Micro::Authorization::PolicyTest < Minitest::Test
   require 'ostruct'
 
-  class StandardBehavior < Minitest::Test
-    class StardardPolicy < Micro::Authorization::Policy
-    end
+  def setup
+    @user = OpenStruct.new(name: 'User', id: 1)
+    @record_a = OpenStruct.new(user_id: @user.id)
+    @record_b = OpenStruct.new(user_id: 2)
+  end
 
-    def test_false_as_the_default_result_to_any_kind_of_query
-      user = {}
-      record = {}
-      policy = StardardPolicy.new(user)
+  def test_false_as_the_default_result_to_any_kind_of_query
+    user = {}
+    record = {}
+    policy = Micro::Authorization::Policy.new(user)
 
-      refute policy.index?
-      refute policy.show?(record)
-      refute policy.show?(record: record)
-      refute policy.show? { record }
-    end
+    refute policy.index?
+    refute policy.show?(record)
+    refute policy.show?(record: record)
+    refute policy.show? { record }
+  end
 
-    def test_non_predicate_method
-      err = assert_raises(NoMethodError) { StardardPolicy.new({}).foo }
-      assert err.message.include?('foo')
+  def test_a_non_predicate_method
+    err = assert_raises(NoMethodError) { Micro::Authorization::Policy.new({}).foo }
+    assert err.message.include?('foo')
+  end
+
+  class PolicyA < Micro::Authorization::Policy
+    def show?
+      user.id == subject.user_id
     end
   end
 
-  class CustomBehavior < Minitest::Test
-    def setup
-      @user = OpenStruct.new(name: 'User', id: 1)
-      @record_a = OpenStruct.new(user_id: @user.id)
-      @record_b = OpenStruct.new(user_id: 2)
+  def test_policy_result_when_receives_a_subject_in_the_initializer
+    policy_context = {user: @user}
+    assert PolicyA.new(policy_context, @record_a).show?
+    refute PolicyA.new(policy_context, @record_b).show?
+  end
+
+  class PolicyB < Micro::Authorization::Policy
+    def show?(record)
+      permissions.to?('visit') && current_user.id == record.user_id
     end
+  end
 
-    class CustomPolicyA < Micro::Authorization::Policy
-      def show?
-        user.id == subject.user_id
-      end
-    end
+  def test_policy_result_when_receives_the_subject_as_a_query_argument
+    role = { 'visit' => { 'only' => %w[users] } }
+    permissions = Micro::Authorization::Permissions.new(role, context: %w[users])
+    policy_context = {user: @user}
 
-    def test_policy_result_when_receives_a_subject_in_the_initializer
-      assert CustomPolicyA.new({user: @user}, @record_a).show?
-      refute CustomPolicyA.new({user: @user}, @record_b).show?
-    end
+    policy = PolicyB.new(policy_context, permissions: permissions)
 
-    class CustomPolicyB < Micro::Authorization::Policy
-      def show?(record)
-        permissions.to?('visit') && current_user.id == record.user_id
-      end
-    end
+    assert policy.show?(@record_a)
+    refute policy.show?(@record_b)
+  end
 
-    def test_policy_result_when_receives_the_subject_as_a_query_argument
-      permissions = Micro::Authorization::Permissions.new(
-        { 'visit' => { 'only' => ['test'] } }, context: ['test']
-      )
+  def test_policy_type
+    assert_kind_of(
+      Class,
+      Micro::Authorization::Policy.type(PolicyA)
+    )
 
-      policy = CustomPolicyB.new({user: @user}, permissions: permissions)
+    # ---
 
-      assert policy.show?(@record_a) == true && policy.show?(@record_b) == false
-    end
+    err = assert_raises(ArgumentError) { Micro::Authorization::Policy.type(Hash) }
+    assert_equal('policy must be a Micro::Authorization::Policy', err.message)
   end
 end
